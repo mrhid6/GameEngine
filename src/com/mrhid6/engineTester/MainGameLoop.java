8e3ea08d25581741509b3d2ac67fd867a41e69b7
mrhid6 <mrhid6@github.com>
2014-09-14 01:47:55 +0100

    Implemented Obj Parser

--- src/com/mrhid6/engineTester/MainGameLoop.java
+++ src/com/mrhid6/engineTester/MainGameLoop.java
@@ -9,6 +9,7 @@ import com.mrhid6.models.RawModel;
 import com.mrhid6.models.TexturedModel;
 import com.mrhid6.renderengine.DisplayManager;
 import com.mrhid6.renderengine.Loader;
+import com.mrhid6.renderengine.OBJLoader;
 import com.mrhid6.renderengine.Renderer;
 import com.mrhid6.shaders.StaticShader;
 import com.mrhid6.textures.ModelTexture;
@@ -25,91 +26,12 @@ public class MainGameLoop {
 		
 		Renderer renderer = new Renderer(shader);
 		
-		float[] vertices = {			
-				-0.5f,0.5f,-0.5f,	
-				-0.5f,-0.5f,-0.5f,	
-				0.5f,-0.5f,-0.5f,	
-				0.5f,0.5f,-0.5f,		
-				
-				-0.5f,0.5f,0.5f,	
-				-0.5f,-0.5f,0.5f,	
-				0.5f,-0.5f,0.5f,	
-				0.5f,0.5f,0.5f,
-				
-				0.5f,0.5f,-0.5f,	
-				0.5f,-0.5f,-0.5f,	
-				0.5f,-0.5f,0.5f,	
-				0.5f,0.5f,0.5f,
-				
-				-0.5f,0.5f,-0.5f,	
-				-0.5f,-0.5f,-0.5f,	
-				-0.5f,-0.5f,0.5f,	
-				-0.5f,0.5f,0.5f,
-				
-				-0.5f,0.5f,0.5f,
-				-0.5f,0.5f,-0.5f,
-				0.5f,0.5f,-0.5f,
-				0.5f,0.5f,0.5f,
-				
-				-0.5f,-0.5f,0.5f,
-				-0.5f,-0.5f,-0.5f,
-				0.5f,-0.5f,-0.5f,
-				0.5f,-0.5f,0.5f
-				
-		};
+		RawModel model = OBJLoader.loadObjModel("res/models/human/Platformer.obj", loader);
 		
-		float[] textureCoords = {
-				
-				0,0,
-				0,1,
-				1,1,
-				1,0,			
-				0,0,
-				0,1,
-				1,1,
-				1,0,			
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0
-
-				
-		};
-		
-		int[] indices = {
-				0,1,3,	
-				3,1,2,	
-				4,5,7,
-				7,5,6,
-				8,9,11,
-				11,9,10,
-				12,13,15,
-				15,13,14,	
-				16,17,19,
-				19,17,18,
-				20,21,23,
-				23,21,22
-
-		};
-		
-		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
-		
-		ModelTexture texture = new ModelTexture(loader.loadTexture("res/textures/test.png"));
+		ModelTexture texture = new ModelTexture(loader.loadTexture("res/models/human/test.png"));
 		TexturedModel texturedModel = new TexturedModel(model, texture);
 		
-		Entity entity = new Entity(texturedModel, new Vector3f(0, 0, -5), 0, 0, 0, 1);
+		Entity entity = new Entity(texturedModel, new Vector3f(0, 0, -10), 0, 0, 0, 1);
 		
 		Camera camera = new Camera();
 		
@@ -118,7 +40,7 @@ public class MainGameLoop {
 			
 			renderer.perpare();
 			//game logic
-			entity.increaseRotation(1, 1, 0);
+			entity.increaseRotation(0, 1, 0);
 			camera.move();
 			
 			shader.start();
